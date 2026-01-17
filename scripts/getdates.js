d445f42750dbe963e932a2620b5f6e300860f273
angelclavoormeno <angeljph123@gmail.com>
2026-01-17 17:18:22 -0500

    W01 Assignment complete

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
